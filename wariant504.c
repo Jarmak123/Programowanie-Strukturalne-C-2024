fb727987c27499c30af7f83566df6e1a2c878506
Jarmak <147035765+Jarmak123@users.noreply.github.com>
2024-03-18 10:10:42 +0100

    wariant 504

---
+++ wariant504.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Wariant 504
+double find_min_pointed_numbers(double *const num1, const double *num2)
+{
+    if(*num1<*num2)
+    {
+        return *num1;
+    }
+    else
+    {
+        return *num2;
+    }
+}
+
+int main()
+{
+    double x = 30;
+    double y = 20;
+    printf("Hello world!\n");
+    printf("Mniejsza wartosc z liczb x i y to: %f\n",find_min_pointed_numbers(&x,&y));
+    return 0;
+}
