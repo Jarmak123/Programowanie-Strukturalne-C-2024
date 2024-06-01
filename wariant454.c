33f40103bffa7548fe75c89af10a927b459de747
Jarmak <147035765+Jarmak123@users.noreply.github.com>
2024-06-01 11:21:36 +0200

    Update wariant454.c

--- wariant454.c
+++ wariant454.c
@@ -13,13 +13,33 @@ int function(struct House tab[], int n)
 {
     int wynik=0;
     int i=0;
-    while(i<n)
+
+
+    if(tab!=NULL)
     {
-        if(tab[i].squareMeters > wynik)
+        while(i<n)
         {
-            wynik=tab[i].squareMeters;
+            if(tab[i].squareMeters < 0 || tab[i].address < 0)
+            {
+                exit(-1);
+            }
+            i=i+1;
         }
-        i=i+1;
+
+        i=0;
+
+        while(i<n)
+        {
+            if(tab[i].squareMeters > wynik)
+            {
+                wynik=tab[i].squareMeters;
+            }
+            i=i+1;
+        }
+    }
+    else
+    {
+        exit(-1);
     }
     return wynik;
 }
@@ -29,7 +49,7 @@ int main()
     struct House domek[3];
 
     domek[0].address="Ketrzynskiego 20";
-    domek[0].squareMeters=34;
+    domek[0].squareMeters=32;
 
     domek[1].address="Mineralna 10";
     domek[1].squareMeters=76;
